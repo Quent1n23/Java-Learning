20e9ea1a14776581d2dc3ab8040a4736f91ad87a
Quent1n23 <1020266436@qq.com>
2024-01-01 23:55:34 +0800

    数组完成，开始面向对象

--- SublimeCode/Chapter06/Homework07.java
+++ SublimeCode/Chapter06/Homework07.java
@@ -8,9 +8,19 @@ public class Homework07{
 		//冒泡排序
 		//要求从小到大
 		int[] arr = {20, -1, 5, 123, 12, 68, };
+		int temp = 0;
+		for(int i = 0; i < arr.length-1; i++){//外层循环，几轮比较
+			for(int j = 0; j < arr.length-1-i; j++){//每轮的比较次数
+				if(arr[j] > arr[j+1]){
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
 
-		for(int i = 0; i < arr.length-1; i++){
-			for(int j = 0; j < )
+		for(int i = 0;  i < arr.length; i++){
+			System.out.print(arr[i] + "\t");
 		}
 		
 	}
