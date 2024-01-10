554d2641ebba3d41fe2524be9d082d21e808410f
Quent1n23 <1020266436@qq.com>
2024-01-10 19:13:54 +0800

    2024.1.10,访问控制修饰符,写错了，更改

--- IDEA_Java_Projects/Chapter08/src/com/hspedu/modifier/A.java
+++ IDEA_Java_Projects/Chapter08/src/com/hspedu/modifier/A.java
@@ -3,7 +3,7 @@ package com.hspedu.modifier;
 访问级别    访问控制修饰符     同类(本类)   同包   子类   不同包
 公开          public               √        √       √      √
 受保护        protected            √        √       √      ×
-默认          没有修饰符           √        ×       ×      ×
+默认          没有修饰符           √        √       ×      ×
 私有          private              √        ×       ×      ×
  */
 public class A {
