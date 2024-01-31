5dfb40ac321cfb5a9fb7aab63d71e122f93d94f1
Quent1n23 <1020266436@qq.com>
2024-01-31 19:46:56 +0800

    2024.1.31,处理流(包装流),模拟修饰器设计模式

---
+++ IDEA_Java_Projects/Chapter19/src/com/hspedu/buffered_/FileReader_.java
@@ -0,0 +1,17 @@
+package com.hspedu.buffered_;
+
+
+/**
+ * @author 丁聪
+ * @version 1.0
+ * 节点流
+ */
+public class FileReader_ extends Reader_ {
+
+        public void readFile() {
+        System.out.println("对文件进行读取...");
+    }
+
+
+
+}
---
+++ IDEA_Java_Projects/Chapter19/src/com/hspedu/buffered_/StringReader_.java
@@ -0,0 +1,13 @@
+package com.hspedu.buffered_;
+
+/**
+ * @author 丁聪
+ * @version 1.0
+ * 节点流
+ */
+public class StringReader_ extends Reader_ {
+    public void readString() {
+        System.out.println("读取字符串..");
+    }
+
+}
