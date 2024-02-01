940486cb61e22c487cdbece00ebc17f5d79989c4
Quent1n23 <1020266436@qq.com>
2024-02-01 22:30:06 +0800

    2024.2.1,坦克大战6

---
+++ IDEA_Java_Projects/Chapter20/src/com/hspedu/tankgame6/TankGame06.java
@@ -0,0 +1,44 @@
+package com.hspedu.tankgame6;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Scanner;
+
+/**
+ * @author 丁聪
+ * @version 1.0
+ */
+public class TankGame06 extends JFrame {
+
+    MyPanel mp = null;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        TankGame06 tankGame06 = new TankGame06();
+    }
+
+    public TankGame06(){
+        System.out.println("请输入选择1: 新游戏 2:继续上局");
+        String key = scanner.next();
+        mp = new MyPanel(key);
+        //启动mp的线程
+        new Thread(mp).start();
+        this.add(mp);
+        this.setSize(1600,900);
+        this.addKeyListener(mp);// 画板mp传入监听器
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        //在JFrame中 增加响应关闭窗口的处理
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                Recorder.keepRecord();
+                System.exit(0);
+            }
+        });
+
+    }
+
+}
